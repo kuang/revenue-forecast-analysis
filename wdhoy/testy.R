de6484daacce9b68f7f9c9cc881299454fe98a23
John F. Davis <davisjf@gmail.com>
2018-05-24 13:32:31 -0400

    stub

--- wdhoy/testy.R
+++ wdhoy/testy.R
@@ -1,1 +1,10 @@
 # just a test thing for reading the excel sheets
+
+# allows reading of excel sheets
+library(readxl)
+# allows writing of excel sheets
+library(writexl)
+
+excel_sheets('sample.xlsx')
+
+input_sheet_1 read_excel("sample.xlsx", sheet=1, col_names = TRUE)
