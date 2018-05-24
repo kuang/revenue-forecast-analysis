d732db0d7188987e1f94959c0ddbd811526a8999
Justin Kuang <zk65@cornell.edu>
2018-05-24 13:51:56 -0400

    data values for used car sale numbers

--- wdhoy/testy.R
+++ wdhoy/testy.R
@@ -9,11 +9,14 @@ excel_sheets('yoy.xlsx')
 
 input_sheet_1 = read_excel("yoy.xlsx", sheet=1, col_names = TRUE)
 input_sheet_1
-input_sheet_1[1,1]  # read col1 row1?
+input_sheet_1[21,6]  # read row1 col1?
 
 
 # template
+
 # what is row,col for upper left corner of rev
+#[14, 4]
 # what lowe right
+#[21, 6]
 
 
