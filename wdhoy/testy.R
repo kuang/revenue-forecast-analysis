ba259ddcddff36f638f7fb080da150bf98c75d2e
Justin Kuang <zk65@cornell.edu>
2018-05-24 13:38:11 -0400

    putting data in correct directory

--- wdhoy/testy.R
+++ wdhoy/testy.R
@@ -5,9 +5,9 @@ library(readxl)
 # allows writing of excel sheets
 library(writexl)
 
-excel_sheets('sample.xlsx')
+excel_sheets('yoy.xlsx')
 
-input_sheet_1 = read_excel("sample.xlsx", sheet=1, col_names = TRUE)
+input_sheet_1 = read_excel("yoy.xlsx", sheet=1, col_names = TRUE)
 input_sheet_1
 input_sheet_1[1,1]  # read col1 row1?
 
